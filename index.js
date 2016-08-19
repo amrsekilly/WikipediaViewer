27decfd12ff9618a42dbd761b5cd11b6fffdec27
amrme <amr.e.dev@gmail.com>
2016-08-19 09:01:35 +0300

    implemented forming the search term

---
+++ index.js
@@ -0,0 +1,20 @@
+// get the homepage
+// https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json
+
+var url = "https://en.wikipedia.org/w/api.php?action=query&titles=";
+
+$('#searchTerm').bind("enterKey",function(e){
+  // perform the request
+  var title = $('#searchTerm').val();
+  var requestURL = url + title + '&prop=revisions&rvprop=content&format=json';
+  console.log(requestURL);
+  // clear the search bar
+  $("#searchTerm").val('');
+});
+
+$('#searchTerm').keyup(function(e){
+    if(e.keyCode == 13)
+    {
+        $(this).trigger("enterKey");
+    }
+});
