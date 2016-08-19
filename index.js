df6b59ffcdcb628a5c5170b0e26fc18e0122796c
amrme <amr.e.dev@gmail.com>
2016-08-19 10:55:51 +0300

    done

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 // get the homepage
 // https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json
 
-var url = "https://en.wikipedia.org/w/api.php?action=query&titles=";
+var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
 
 $('#searchTerm').bind("enterKey",function(e){
   // perform the request
   var title = encodeURIComponent($('#searchTerm').val());
-  var requestURL = url + title + '&prop=revisions&rvprop=content&format=json';
+  var requestURL = url + title + '&callback=JSON_CALLBACK';
+
+
+  function getPages(data) {
+    // https://en.wikipedia.org/?curid=13834
+
+    var pages = data.query.pages;
+    console.log(pages);
+
+    for (var page in pages) {
+      if (pages.hasOwnProperty(page)) {
+        var currentPage = pages[page];
+        $("#results").append("<div><a target='_blank' href='https://en.wikipedia.org/?curid="+currentPage.pageid+"'><h3>"+ currentPage.title +"</h3><p>"+currentPage.extract+"</p></a></div>");
+      }
+    }
+  }
 
   // get the JSON data
-  $.getJSON(requestURL).done(function(data) {
-    console.log($.parseJSON(data));
+  $.ajax({
+     dataType: "jsonp",
+     url:  requestURL,
+     success: getPages
   });
 
   // clear the search bar
   $("#searchTerm").val('');
+
+  // reset the html for the results
+  $("#results").html("");
 });
 
 $('#searchTerm').keyup(function(e){
