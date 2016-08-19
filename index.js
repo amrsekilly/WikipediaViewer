accab6c8c1d642623d2a01a52d282535106df4b2
amrme <amr.e.dev@gmail.com>
2016-08-19 09:20:24 +0300

    test

---
+++ index.js
@@ -0,0 +1,25 @@
+// get the homepage
+// https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json
+
+var url = "https://en.wikipedia.org/w/api.php?action=query&titles=";
+
+$('#searchTerm').bind("enterKey",function(e){
+  // perform the request
+  var title = encodeURIComponent($('#searchTerm').val());
+  var requestURL = url + title + '&prop=revisions&rvprop=content&format=json';
+
+  // get the JSON data
+  $.getJSON(requestURL).done(function(data) {
+    console.log($.parseJSON(data));
+  });
+
+  // clear the search bar
+  $("#searchTerm").val('');
+});
+
+$('#searchTerm').keyup(function(e){
+    if(e.keyCode == 13)
+    {
+        $(this).trigger("enterKey");
+    }
+});
